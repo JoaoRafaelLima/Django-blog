d637b8f5d4339647ad52013d4b9ee1adb3286854
João Rafael <jrafael0618@gmail.com>
2020-07-04 12:24:11 -0300

    update

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('posts/', views.list_posts, name="list-posts"),
+    path('post/<int:idp>', views.read_post, name="post"),
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 
 def home(request):
-    return render(request, "base.html")
+    current_post = Post.objects.all().order_by('-created_at')[0]
+    return render(request, "posts/home.html", {"current_post": current_post})
+
+def  read_post(request, idp):
+    current_post = Post.objects.filter(id=idp)[0]
+    return render(request, "posts/home.html", {"current_post": current_post})
+
+def list_posts(request):
+    posts_title = Post.objects.values('title', 'id')
+    return render(request, "posts/posts.html", {"posts_list": posts_title})
