ed93699dae9948c7337898fcb145bc115b5b4df2
João Rafael <jrafael0618@gmail.com>
2020-07-07 23:01:12 -0300

    paginacao implementada

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from .models import Post
 # Create your views here.
 
@@ -13,5 +14,12 @@ def  read_post(request, idp):
     return render(request, "posts/home.html", {"current_post": current_post})
 
 def list_posts(request):
-    posts = Post.objects.values('title', 'id').order_by("-created_at")
+    posts_list = Post.objects.values('title', 'id').order_by("-created_at")
+    paginator = Paginator(posts_list, 3)
+    try:
+        #page = int(request.GET.get('page', '1'))
+        page = request.GET.get("page")
+    except ValueError:
+        page = 1
+    posts = paginator.get_page(page)
     return render(request, "posts/posts.html", {"posts_list": posts})
